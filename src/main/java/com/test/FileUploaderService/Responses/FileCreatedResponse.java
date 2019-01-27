96a14dffbc663220a5207216ebc0cc0d14231261
Logeshkumar <logeshkr@thoughtworks.com>
2019-01-27 23:55:54 +0530

    Created Response class to return uploaded file id, which will be used by controller

---
+++ src/main/java/com/test/FileUploaderService/Responses/FileCreatedResponse.java
@@ -0,0 +1,10 @@
+package com.test.FileUploaderService.Responses;
+
+public class FileCreatedResponse {
+
+    public String id;
+
+    public FileCreatedResponse(String id){
+        this.id = id;
+    }
+}
